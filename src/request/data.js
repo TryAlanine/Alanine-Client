bb0a5623f00c4f2227f4323bdaaa3393e7c0c714
Lawrence Ye <leaferx@outlook.com>
2018-08-06 23:10:14 +0800

    update

--- src/request/data.js
+++ src/request/data.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default function getdata(serverURL) {
-  const dataURL = `${serverURL}`;
-  const data = axios.get(dataURL);
-  return JSON.parse(data);
+  const meta = axios.get(`${serverURL}/api/meta`);
+  return JSON.parse(meta);
 }
